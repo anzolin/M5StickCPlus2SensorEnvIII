3e7752dac67663b7eeb7cd814576c4ae8965bc69
Anzolin <diego@anzolin.com.br>
2025-01-02 14:58:05 -0300

    First commit

---
+++ M5StickCPlus2SensorEnvIII.ino
@@ -0,0 +1,122 @@
+/**
+ * @file M5StickCPlus2SensorEnvIII.ino
+ * @author Diego Anzolin (diego@anzolin.com.br)
+ * @brief Example of how you can pull temperature, humidity and pressure data from the ENV III sensor paired with the M5StickCPlus2.
+ * @version 0.1
+ * @date 2025-01-02
+ * @Hardwares: M5StickCPlus2, Unit ENV-III (https://docs.m5stack.com/en/unit/envIII)
+ */
+
+#include "M5StickCPlus2.h"
+#include "M5Unified.h"
+#include "M5UnitENV.h"
+
+SHT3X sht30;      // temperature and humidity sensor
+QMP6988 qmp6988;  // pressure sensor
+
+float temp = 0.0;
+float hum = 0.0;
+float pressure = 0.0;
+
+void GetMeasurements() {
+  StickCP2.Power.setLed(255);
+  StickCP2.Display.setTextColor(WHITE);
+
+  // Get new updated values from our sensor
+  if (qmp6988.update()) {
+    pressure = qmp6988.calcPressure();
+  }
+
+  if (sht30.update()) {    // Obtain the data of sht30.
+    temp = sht30.cTemp;    // Store the temperature obtained from sht30.
+    hum = sht30.humidity;  // Store the humidity obtained from the sht30.
+  } else {
+    temp = 0, hum = 0;
+  }
+
+  // Sometimes this sensor fails, and if we get an invalid reading it's best to just restart the controller to clear it out
+  if (pressure < 950) {
+    ESP.restart();
+  }
+
+  // Sometimes this sensor fails, and if we get an invalid reading it's best to just restart the controller to clear it out
+  if (pressure / 100 > 1200) {
+    ESP.restart();
+  }
+
+  Serial.printf("Temp: %2.1f °C \r\nHumidity: %2.0f%%  \r\nPressure: %2.0f hPa\r\n\n", temp, hum, pressure / 100);
+
+  StickCP2.Display.setCursor(10, 30);
+  StickCP2.Display.printf("Temperature: %2.1f°C \n", temp);
+
+  StickCP2.Display.setCursor(10, 40);
+  StickCP2.Display.printf("Humidity: %2.0f%% \n", hum);
+
+  StickCP2.Display.setCursor(10, 50);
+  StickCP2.Display.printf("Pressure: %2.0f hPa \n", pressure / 100);
+
+  // Gather some internal data as well, about battery states, voltages, charge rates and so on
+  int bat_volt = StickCP2.Power.getBatteryVoltage();
+  Serial.printf("Battery Volt: %dmv \n", bat_volt);
+
+  int bat_current = StickCP2.Power.getBatteryCurrent();
+  Serial.printf("Battery Current: %dmv \n", bat_current);
+
+  int bat_level = StickCP2.Power.getBatteryLevel();
+  Serial.printf("Battery Level: %d% \n\n", bat_level);
+
+  StickCP2.Power.setLed(0);
+}
+
+void setup() {
+  // Initialize the M5StickCPlus2
+  auto cfg = M5.config();
+  StickCP2.begin(cfg);
+
+  // Setup the serial port for debugging
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Initializing...");
+
+  Wire.begin();
+
+  if (!qmp6988.begin(&Wire, QMP6988_SLAVE_ADDRESS_L, 32, 33, 400000U)) {
+    Serial.println("Couldn't find QMP6988");
+    while (1) delay(1);
+  }
+
+  if (!sht30.begin(&Wire, SHT3X_I2C_ADDR, 32, 33, 400000U)) {
+    Serial.println("Couldn't find SHT30");
+    while (1) delay(1);
+  }
+
+  StickCP2.Display.fillScreen(BLACK);
+  StickCP2.Display.setRotation(3);
+
+  Serial.println("Initialized.");
+}
+
+void DisplayScreen() {
+  StickCP2.Display.setTextColor(GREENYELLOW);
+  StickCP2.Display.setCursor(10, 10);
+  StickCP2.Display.println("M5StickCPlus2 with Sensor ENV III");
+
+  StickCP2.Display.setTextColor(GOLD);
+  StickCP2.Display.setCursor(10, 120);
+  StickCP2.Display.println("Press 'A' to measure.");
+}
+
+void loop() {
+  StickCP2.update();
+
+  DisplayScreen();
+
+  if (StickCP2.BtnA.wasReleased()) {
+    Serial.println("Button A was released.");
+
+    StickCP2.Speaker.tone(8000, 400);
+    StickCP2.Display.fillScreen(BLACK);
+    DisplayScreen();
+    GetMeasurements();
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,62 @@
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+[![Contributors][contributors-shield]][contributors-url]
+[![Watchers][watchers-shield]][watchers-url]
+[![Stargazers][stars-shield]][stars-url]
+[![Forks][forks-shield]][forks-url]
+[![Issues][issues-shield]][issues-url]
+[![MIT License][license-shield]][license-url]
+
+---
+
 # M5StickCPlus2SensorEnvIII
+
+![Alt text](result.jpg?raw=true "M5StickCPlus2")
+
 Example of how you can pull temperature, humidity and pressure data from the ENV III sensor paired with the M5StickCPlus2.
+
+## Hardware used:
+- [M5 Cardputer](https://docs.m5stack.com/en/core/M5StickC%20PLUS2)
+- [Unit ENV-III](https://docs.m5stack.com/en/unit/envIII)
+
+## License
+
+This project is [MIT Licensed](https://github.com/anzolin/M5StickCPlus2SensorEnvIII/blob/master/LICENSE).
+
+  
+## About the author
+
+Hello everyone, my name is Diego Anzolin Ferreira. I'm a .NET developer from Brazil. I hope you will enjoy this project as much as I enjoy developing it. If you have any problems, you can post a [GitHub issue](https://github.com/anzolin/M5StickCPlus2SensorEnvIII/issues). You can reach me out at diego@anzolin.com.br.
+
+
+## Donate
+  
+Want to help me keep creating open source projects, make a donation:
+
+[![Donate](https://img.shields.io/badge/Donate-PayPal-green.svg?style=for-the-badge)](https://www.paypal.com/donate?business=DN2VPNW42RTXY&no_recurring=0&currency_code=BRL) [![Donate](https://img.shields.io/badge/-buy_me_a%C2%A0coffee-gray?logo=buy-me-a-coffee&style=for-the-badge)](https://www.buymeacoffee.com/anzolin)
+
+Thank you!
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/anzolin/M5StickCPlus2SensorEnvIII.svg?style=for-the-badge
+[contributors-url]: https://github.com/anzolin/M5StickCPlus2SensorEnvIII/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/anzolin/M5StickCPlus2SensorEnvIII.svg?style=for-the-badge
+[forks-url]: https://github.com/anzolin/M5StickCPlus2SensorEnvIII/network/members
+[watchers-shield]: https://img.shields.io/github/watchers/anzolin/M5StickCPlus2SensorEnvIII.svg?style=for-the-badge
+[watchers-url]: https://github.com/anzolin/M5StickCPlus2SensorEnvIII/watchers
+[stars-shield]: https://img.shields.io/github/stars/anzolin/M5StickCPlus2SensorEnvIII.svg?style=for-the-badge
+[stars-url]: https://github.com/anzolin/M5StickCPlus2SensorEnvIII/stargazers
+[issues-shield]: https://img.shields.io/github/issues/anzolin/M5StickCPlus2SensorEnvIII.svg?style=for-the-badge
+[issues-url]: https://github.com/anzolin/M5StickCPlus2SensorEnvIII/issues
+[license-shield]: https://img.shields.io/github/license/anzolin/M5StickCPlus2SensorEnvIII.svg?style=for-the-badge
+[license-url]: https://github.com/anzolin/M5StickCPlus2SensorEnvIII/blob/master/LICENSE.txt
+
